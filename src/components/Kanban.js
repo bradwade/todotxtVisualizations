302a3a3bbc5f65981a01d50535da62b8dba84e8c
Chris DeLuca <code@chrisdeluca.me>
2020-06-07 18:02:03 -0400

    Convert to Kanban react hooks

--- src/components/Kanban.js
+++ src/components/Kanban.js
@@ -1,17 +1,15 @@
 import React from 'react';
 
-class Kanban extends React.Component {
-  render() {
-    return (
-      <div className="kanban-view">
-        <div className="column">a column</div>
-        <div className="column">a column</div>
-        <div className="column">a column</div>
-        <div className="column">a column</div>
-        <div className="column">a column</div>
-      </div>
-    );
-  }
+function Kanban(props) {
+  return (
+    <div className="kanban-view">
+      <div className="column">a column</div>
+      <div className="column">a column</div>
+      <div className="column">a column</div>
+      <div className="column">a column</div>
+      <div className="column">a column</div>
+    </div>
+  );
 }
 
 export default Kanban;
