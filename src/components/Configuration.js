b937cf79c36880c8364b0d80d88e392a3fb1e837
Brad Wade <brad@project47.com>
2021-11-25 22:27:36 -0800

    Styling and layout for the Settings/Configuration page.

--- src/components/Configuration.js
+++ src/components/Configuration.js
@@ -1,6 +1,6 @@
 import React, {useState} from 'react';
 import DragAndDrop from './DragAndDrop';
-import { Typography, TextareaAutosize, Button, Container } from '@material-ui/core';
+import { Divider, Typography, TextareaAutosize, Button, Container, Card, Paper, Grid, List, ListItemText } from '@material-ui/core';
 import CloudUploadIcon from '@material-ui/icons/CloudUpload';
 
 const Configuration = (props) => {
@@ -35,40 +35,75 @@ const Configuration = (props) => {
     <div className="configuration">
       <DragAndDrop handleDrop={handleDrop}>
         <Container>
-          <Typography variant="h4">Load Data</Typography>
-          <Typography variant="body1" component="div">
-            <p>Browse for your todo.txt file (or drag-and-drop a file onto this window).</p>
-            <form>
-              <input multiple type="file" name="file" onChange={handleFileInput}/>
-            </form>
-            <Typography variant="h4">Files</Typography>
-            <div>File(s) being used: {Array.from(files).map(file => file.name).join(", ")}</div>
-            <Typography variant="h4">Categories</Typography>
+
+          <Grid
+            container
+            direction="row"
+            justifyContent="space-between"
+            alignItems="stretch"
+            spacing={4}
+          >
+            <Grid item xs={12}>
+              <Typography variant="h3">Settings</Typography>
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <Card className="settings-card">
+              <Typography variant="h5">Load Data</Typography>
+              <Typography variant="body2" gutterBottom={true}>
+                Browse for your todo.txt file (or drag-and-drop a file onto this window).
+                </Typography>
+              <form className="file-upload-button">
+                <input multiple type="file" name="file" onChange={handleFileInput}/>
+              </form>
+              
+              {Array.from(files).length > 0 &&
+                <>
+                  <Divider className="file-upload-divider" />
+                  <Typography variant="h6">Files being used:</Typography>
+                </>
+              }
+              <List>{Array.from(files).map((file) => (
+                <ListItemText>{file.name}</ListItemText>
+              ))}</List>
+              </Card>
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <Card className="settings-card">
+              <Typography variant="h5">Categories</Typography>
+              <Divider />
               {Object.entries(props.categoriesList).map(([key, value]) => {
                 return (
                   <div key={key} className="filter-section">
-                    <Typography variant="h5">{key}</Typography>
-                      {value.map((category, index) => <li key={index}>{category}</li>)}
+                    <Typography variant="h6">{key}</Typography>
+                    <List>
+                      {value.map((category, index) => <ListItemText key={index}>{category}</ListItemText>)}
+                    </List>
+                    <Divider />
                   </div>
                 )
               })}
-            <Typography variant="h4">Raw todo.txt</Typography>
-            {/* <form>
-            <TextareaAutosize
-              aria-label="todo.txt raw text"
-              placeholder="You can manually enter todo items here."
-              defaultValue={props.todoBlob}
-            />
-              <Button
-                variant="contained"
-                color="primary"
-              >
-                Submit
-              </Button>
-            </form> */}
-            <pre id="textDisplayArea">{props.todoBlob}</pre>
-          </Typography>
-
+              </Card>
+            </Grid>
+            <Grid item xs={12}>
+              <Card className="settings-card">
+              <Typography variant="h5">Raw todo.txt</Typography>
+              {/* <form>
+              <TextareaAutosize
+                aria-label="todo.txt raw text"
+                placeholder="You can manually enter todo items here."
+                defaultValue={props.todoBlob}
+              />
+                <Button
+                  variant="contained"
+                  color="primary"
+                >
+                  Submit
+                </Button>
+              </form> */}
+              <pre id="textDisplayArea">{props.todoBlob}</pre>
+              </Card>
+            </Grid>
+          </Grid>
         </Container>
       </DragAndDrop>
     </div>
